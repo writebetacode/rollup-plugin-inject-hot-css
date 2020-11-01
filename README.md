f731602dd9f909665a75fe9995355621a7db6872
writebetacode <scholle.mv@protonmail.com>
2020-10-31 22:41:41 -0400

    adding in the actual rollup-plugin and setting up github action to push to npmjs on tagged versions

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # rollup-plugin-inject-hot-css
-rollup plugin to inject module code to enable hot reloading of css files
+
+A simple CSS Hot Module Replacement code injector for [Rollup](https://rollupjs.org) when used with [Nollup](https://github.com/PepsRyuu/nollup).  This should allow you to use any CSS loader and still have access to Hot css when developing.
+
+## Installation
+
+`npm install -D rollup-plugin-inject-hot-css`
+
+## Usage with _rollup-plugin-styles_
+
+```js
+// rollup.config.js
+import styles from "rollup-plugin-styles";
+
+const injectHotCSS = require("rollup-plugin-inject-hot-css"),
+  isProduction = process.env.NODE_ENV === "pproduction";
+
+export default {
+  ...,
+  plugins: [{
+    styles({
+      mode: "extract",
+      minimize: true,
+      sourceMap: isProduction ? true : "inline",
+    }),
+    !isProduction && injectHotCSS()
+  }]
+};
+
+```
+
+## Options
+
+* extensions - array defining the file extensions this plugin should use to determine which files to inject the css hot module replacement code into.
+  * Defaults to `[ ".css", ".scss", ".sass", ".less", ".styl" ]`
+
+## Hot Module Replacement
+
+This plugin expects there to be a _link_ tag inside the index.html file in order
+to replace the CSS appropriately.
+
+`<link rel="stylesheet" type="text/css" href="/styles.css">`
+
+When file changes are made, the link tag is replaced by appending a timestamp to the end of the href. This forces the browser to download the file again without needing to refresh the page.
+
+## Notes
+
+* If you are looking for an all in one css loader that also incorporates the css hot module replacement logic, I would suggest looking into the [rollup-plugin-hot-css](https://github.com/PepsRyuu/rollup-plugin-hot-css) plugin.
---
+++ index.js
@@ -0,0 +1,67 @@
+function hmrCode(filename) {
+  return `
+  ;(() => {
+    window.__inject_hot_css_reload = () => {
+      if(window.__inject_hot_css_link_timeout) {
+        cancelAnimationFrame(window.__inject_hot_css_link_timeout);
+      }
+
+      window.__inject_hot_css_link_timeout = requestAnimationFrame(() => {
+        const selector = 'link[href*="${filename}"]',
+          link = document.querySelector(selector);
+
+        if(link) {
+          const href = link.getAttribute('href').split("?")[0] + "?" + Date.now(),
+            nextLink = document.createElement("link");
+
+          nextLink.setAttribute('rel', 'stylesheet');
+          nextLink.setAttribute('type', 'text/css');
+          nextLink.setAttribute('href', href);
+          nextLink.onload = () => link.remove();
+          document.head.appendChild(nextLink);
+        }
+      });
+    }
+  })();
+  `;
+}
+
+export default function injectHotCSS (options = {}) {
+  const path = require("path"),
+    defaultExtensions = [ ".css", ".scss", ".sass", ".less", ".styl" ],
+    extensions = options.extensions || defaultExtensions;
+
+  return {
+    name: "injectHotCSS",
+
+    transform: async function transform(_, id) {
+      if(!extensions.includes(path.extname(id))) return
+
+      return `
+        if(module && module.hot) {
+          window.__inject_hot_css_reload();
+          module.hot.accept(window.__inject_hot_css_reload);
+          module.hot.dispose(window.__inject_hot_css_reload);
+        }
+      `;
+    },
+
+    generateBundle(_, bundle) {
+      let entryFile, cssFilename;
+
+      for(const [filename, file] of Object.entries(bundle)) {
+        if(file.isEntry) {
+          entryFile = file;
+        }
+
+        if(filename.includes(".css") && !filename.includes(".map")) {
+          cssFilename = filename;
+        }
+      }
+
+      if(entryFile) {
+        entryFile.code = hmrCode(cssFilename) + entryFile.code;
+      }
+    }
+  };
+};
