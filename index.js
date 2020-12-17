012f32be369820dfb9c63af93f76c4daff8d56b3
writebetacode <scholle.mv@protonmail.com>
2020-12-17 15:13:06 -0500

    adding fix for when code-splitting generates a secondary css file

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
 const path = require("path"),
   defaultExtensions = [ ".css", ".scss", ".sass", ".less", ".styl" ];
 
-function hmrCode(filename) {
+function hmrCode(filenames) {
   return `
   ;(() => {
+    let filenames = ["${filenames.join('","')}"];
+
     window.__inject_hot_css_reload = () => {
       if(window.__inject_hot_css_link_timeout) {
         cancelAnimationFrame(window.__inject_hot_css_link_timeout);
       }
 
       window.__inject_hot_css_link_timeout = requestAnimationFrame(() => {
-        const selector = 'link[href*="${filename}"]',
+        for (let filename of filenames) {
+          const selector = 'link[href*="'+filename+'"]',
           link = document.querySelector(selector);
 
-        if(link) {
-          const href = link.getAttribute('href').split("?")[0] + "?" + Date.now(),
-            nextLink = document.createElement("link");
+          if (link) {
+            const href =
+              link.getAttribute('href').split("?")[0] + "?" + Date.now(),
+              nextLink = document.createElement("link");
 
-          nextLink.setAttribute('rel', 'stylesheet');
-          nextLink.setAttribute('type', 'text/css');
-          nextLink.setAttribute('href', href);
-          nextLink.onload = () => link.remove();
-          document.head.appendChild(nextLink);
+            nextLink.setAttribute('rel', 'stylesheet');
+            nextLink.setAttribute('type', 'text/css');
+            nextLink.setAttribute('href', href);
+            nextLink.onload = () => link.remove();
+            document.head.appendChild(nextLink);
+          }
         }
       });
     }
@@ -36,7 +41,7 @@ module.exports = function injectHotCSS(options = {}) {
     name: "injectHotCSS",
 
     transform: async function transform(_, id) {
-      if(!extensions.includes(path.extname(id))) return
+      if (!extensions.includes(path.extname(id))) return
 
       return `
         if(module && module.hot) {
@@ -48,20 +53,23 @@ module.exports = function injectHotCSS(options = {}) {
     },
 
     generateBundle(_, bundle) {
-      let entryFile, cssFilename;
+      let entryFile,
+        cssFilenames = [];
 
-      for(const [filename, file] of Object.entries(bundle)) {
-        if(file.isEntry) {
+      for (const [filename, file] of Object.entries(bundle)) {
+        if (file.isEntry) {
           entryFile = file;
         }
 
-        if(filename.includes(".css") && !filename.includes(".map")) {
-          cssFilename = filename;
+        if (filename.includes(".css") && !filename.includes(".map")) {
+          if (!cssFilenames.includes(filename)) {
+            cssFilenames.push(filename)
+          }
         }
       }
 
-      if(entryFile) {
-        entryFile.code = hmrCode(cssFilename) + entryFile.code;
+      if (entryFile) {
+        entryFile.code = hmrCode(cssFilenames) + entryFile.code;
       }
     }
   };
